8c8e2350138db8c62b17c047c3fd9e1d5e20fb80
Josh Munday <97485231+JDM210@users.noreply.github.com>
2022-04-27 19:48:23 -0400

    Add project files.

---
+++ DataArrays/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace arrayexample
+{
+    class dotw
+    {
+        public static void Main()
+        {
+            string[] weekDays;
+
+            weekDays = new string[] {"Sun", "Mon", "Tue", "Wed",
+                                       "Thu", "Fri", "Sat"};
+
+            foreach (string day in weekDays)
+                Console.Write(day + " ");
+        }
+    }
+}
